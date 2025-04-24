557dcf6919763c9f99785de6a4e5e6a81adf8bde
maximilian1741 <maximum0mars@gmail.com>
2025-04-24 20:23:13 +0000

    code: return Fizz for 3

--- src/main/java/com/example/project/FizzBuzzApp.java
+++ src/main/java/com/example/project/FizzBuzzApp.java
@@ -1,10 +1,14 @@
 package com.example.project;
 
 public class FizzBuzzApp {
-    public static String fizzBuzz(){
-        int num = 1;
-        num += 1;
-        return Integer.toString(num);
+    public static String fizzBuzz(int num){
+        if (num == 3){
+            return "Fizz";
+        }
+        else {
+            return Integer.toString(num);
+        }
+        
     }
     
 }
--- src/test/java/com/example/project/FizzBuzzTest.java
+++ src/test/java/com/example/project/FizzBuzzTest.java
@@ -2,9 +2,17 @@ package com.example.project;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.beans.Transient;
+
 public class FizzBuzzTest {
     @Test
     public void returnTest(){
-        assertEquals(FizzBuzzApp.fizzBuzz(), "Fizz");
+        assertEquals(FizzBuzzApp.fizzBuzz(1), "1");
+    }
+
+    @Test 
+    public void returnFizzTest(){
+        int num = 3;
+        assertEquals(FizzBuzzApp.fizzBuzz(3), "Fizz");
     }
 }
