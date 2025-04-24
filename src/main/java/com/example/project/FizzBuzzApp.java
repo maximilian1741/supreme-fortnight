6d037f251c7279f3a85348a043c6c5f9b67355a4
maximilian1741 <maximum0mars@gmail.com>
2025-04-24 20:26:15 +0000

    clean: fizzbuzz function

--- src/main/java/com/example/project/FizzBuzzApp.java
+++ src/main/java/com/example/project/FizzBuzzApp.java
@@ -2,15 +2,10 @@ package com.example.project;
 
 public class FizzBuzzApp {
     public static String fizzBuzz(int num){
-        if (num == 3){
-            return "Fizz";
-        }
-        else if (num == 5){
-            return "Buzz";
-        }
-        else {
-            return Integer.toString(num);
-        }
+        
+        if (num == 3){return "Fizz";}
+        else if (num == 5){return "Buzz";}
+        else {return Integer.toString(num);}
         
     }
     
