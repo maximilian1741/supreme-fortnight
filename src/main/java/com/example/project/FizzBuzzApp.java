c92ecf57e28fcd3daaadeff26d0bf729ae10aa4b
maximilian1741 <maximum0mars@gmail.com>
2025-04-24 20:47:48 +0000

    code: return fizzbuzz for multiples of both

--- src/main/java/com/example/project/FizzBuzzApp.java
+++ src/main/java/com/example/project/FizzBuzzApp.java
@@ -2,7 +2,10 @@ package com.example.project;
 
 public class FizzBuzzApp {
     public static String fizzBuzz(int num){
-        if (num % 3 == 0){
+        if(num%3==0 && num%5==0){
+            return "FizzBuzz";
+        }
+        else if (num % 3 == 0){
             return "Fizz";
         }
         else if (num % 5 == 0){
