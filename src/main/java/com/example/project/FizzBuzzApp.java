518220184e328d1b1394b3d528f1a2ebd86ef330
maximilian1741 <maximum0mars@gmail.com>
2025-04-24 20:38:16 +0000

    redesign: ignore last commit oops

--- src/main/java/com/example/project/FizzBuzzApp.java
+++ src/main/java/com/example/project/FizzBuzzApp.java
@@ -2,10 +2,15 @@ package com.example.project;
 
 public class FizzBuzzApp {
     public static String fizzBuzz(int num){
-        
-        if (num == 3){return "Fizz";}
-        else if (num == 5){return "Buzz";}
-        else {return Integer.toString(num);}
+        if (num == 3){
+            return "Fizz";
+        }
+        else if (num == 5){
+            return "Buzz";
+        }
+        else {
+            return Integer.toString(num);
+        }
         
     }
     
--- src/test/java/com/example/project/FizzBuzzTest.java
+++ src/test/java/com/example/project/FizzBuzzTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.beans.Transient;
 
 public class FizzBuzzTest {
+    
     @Test
     public void returnTest(){
         assertEquals(FizzBuzzApp.fizzBuzz(1), "1");
@@ -17,6 +18,8 @@ public class FizzBuzzTest {
 
     @Test 
     public void returnBuzzTest(){
-        assertEquals(FizzBuzzApp.fizzBuzz(5), "Fizz");
+        assertEquals(FizzBuzzApp.fizzBuzz(5), "Buzz");
     }
+
+
 }
